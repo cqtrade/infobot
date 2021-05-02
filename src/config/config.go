adde3292f7598f63463d4e29e9d651907671d843
Sander Mets <sandermets@live.com>
2021-05-02 12:10:34 +0300

    :sparkles: Add sending text notification to Discord

--- src/config/config.go
+++ src/config/config.go
@@ -3,6 +3,7 @@ package config
 import "github.com/spf13/viper"
 
 type Config interface {
+	GetDiscordEnabled() bool
 	GetDiscordChBtcVibe() string
 	GetDiscordChRandomIdeas() string
 	GetDiscordChByChName(string) string
@@ -10,6 +11,7 @@ type Config interface {
 }
 
 type conf struct {
+	discordEnabled       bool
 	discordChBtcVibe     string
 	discordChRandomIdeas string
 	serverUrl            string
@@ -20,10 +22,15 @@ func New() Config {
 	viper.ReadInConfig()
 	viper.AutomaticEnv()
 	return &conf{
+		discordEnabled:       viper.GetBool("DISCORD_ENABLED"),
 		discordChBtcVibe:     viper.GetString("DISCORD_CH_BTC_VIBE"),
 		discordChRandomIdeas: viper.GetString("DISCORD_CH_RANDOM_IDEAS"),
 		serverUrl:            viper.GetString("SERVER_URL"),
 	}
+}
+
+func (c *conf) GetDiscordEnabled() bool {
+	return c.discordEnabled
 }
 
 func (c *conf) GetDiscordChBtcVibe() string {
--- src/service/discord.go
+++ src/service/discord.go
@@ -1,23 +1,59 @@
 package service
 
-import "github.com/cqtrade/infobot/src/config"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/cqtrade/infobot/src/config"
+)
 
 type DiscordService interface {
 	SendTextMessage(msg string)
+	sendNotification(ch string, message string)
 }
 
 type discordService struct {
-	cfg config.Config
+	cfg        config.Config
+	httpClient *http.Client
 }
 
 func NewDiscordService(cfg config.Config) DiscordService {
+	httpClient := &http.Client{Timeout: 10 * time.Second}
 	return &discordService{
-		cfg: cfg,
+		cfg:        cfg,
+		httpClient: httpClient,
 	}
 }
 
+func (ds *discordService) sendNotification(ch string, message string) {
+	if !ds.cfg.GetDiscordEnabled() {
+		return
+	}
+
+	go func(webhookUrl string, msg string, client *http.Client) {
+		type notificationBody struct {
+			Content string `json:"content"`
+		}
+
+		reqBody := notificationBody{Content: msg}
+
+		reqBodyBytes, _ := json.Marshal(reqBody)
+		req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(reqBodyBytes))
+		if err != nil {
+			println("ERROR preparing discord payload" + err.Error())
+		} else {
+			req.Header.Add("Content-Type", "application/json")
+			resp, err := client.Do(req)
+			if resp.StatusCode == 204 && err == nil {
+				return
+			}
+			println("ERROR logger http " + err.Error())
+		}
+	}(ch, message, ds.httpClient)
+}
+
 func (ds *discordService) SendTextMessage(msg string) {
-	println("Send")
-	println(ds.cfg.GetDiscordChRandomIdeas())
-	println(msg)
+	ds.sendNotification(ds.cfg.GetDiscordChRandomIdeas(), msg)
 }
