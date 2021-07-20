602bae66043001775dcc92941088a99a05f3fdba
Sander Mets <sandermets@live.com>
2021-07-20 16:17:48 +0300

    :sparkles: Switch to USD from USDT

--- src/ftxtrade/ftxtrade.go
+++ src/ftxtrade/ftxtrade.go
@@ -24,43 +24,72 @@ func (ft *FtxTrade) BuyEthBull(subAcc string) {
 	key := ft.cfg.FTXKey
 	secret := ft.cfg.FTXSecret
 	client := ftx.New(key, secret, subAcc)
-	sBalances, err := client.GetSubaccountBalances(subAcc)
-	if err != nil {
-		fmt.Println(err)
-	}
-	equity := 0.0
-	if sBalances.Success {
-		for _, balance := range sBalances.Result {
-			if balance.Coin == "USDT" {
-				equity = balance.Free
-			}
-		}
-	}
-	fmt.Println("Available in test1 USDT", equity)
-	market := "ETHBULL/USDT"
+	equity, _ := ft.CheckBalanceUSD("test1", client)
+	market := "ETHBULL/USD"
 	positionSize := ft.cfg.PositionSize
 	profitPercentage := ft.cfg.ProfitPercentage
 	if equity > positionSize {
-		candles, _ := client.GetHistoricalPricesLatest(market, 60, 1)
-		if len(candles.Result) > 0 {
-			candle := candles.Result[0]
-			fmt.Println(candle.Close)
-			marketPrice := candle.Close
-			size := math.Round((positionSize/candle.Close)*10000) / 10000
-			fmt.Println("Buy market")
-			fmt.Println(market)
-			fmt.Println(size)
-			orderMarket, _ := client.PlaceMarketOrder(market, "buy", "market", size)
-			fmt.Println(orderMarket)
-			if orderMarket.Success == true {
-				sizeTP := math.Round((size/2)*10000) / 10000 // sell 50%
-				priceTP := math.Round((marketPrice+marketPrice*profitPercentage)*10) / 10
-				fmt.Println("TP")
-				fmt.Println(sizeTP)
-				fmt.Println(priceTP)
-				orderTP, _ := client.PlaceOrder(market, "sell", priceTP, "limit", sizeTP, false, false, false)
-				fmt.Println(orderTP)
+		marketPrice, _ := ft.GetLastPriceForMarket(market, client)
+		if marketPrice <= 0.0 {
+			fmt.Println(market, " marketPrice price not greater than zero: ", marketPrice)
+			return
+		}
+
+		size := math.Round((positionSize/marketPrice)*10000) / 10000
+		fmt.Println("Buy market\t", market, "\t", size)
+		orderMarket, err := client.PlaceMarketOrder(market, "buy", "market", size)
+		if err != nil {
+			fmt.Println("ERROR with Market order ", err)
+		}
+		fmt.Println(orderMarket)
+		if orderMarket.Success == true {
+			sizeTP := math.Round((size/2)*10000) / 10000 // sell 50%
+			marketPrice, _ := ft.GetLastPriceForMarket(market, client)
+			priceTP := math.Round((marketPrice+marketPrice*profitPercentage)*10) / 10
+			fmt.Println("TP size: ", sizeTP, " price: ", priceTP)
+			orderTP, err := client.PlaceOrder(market, "sell", priceTP, "limit", sizeTP, false, false, false)
+			fmt.Println(orderTP)
+			if err != nil {
+				fmt.Println("ERROR with TP order ", err)
+			} else if orderTP.Success {
+				fmt.Println("FLOW SUCCESS")
 			}
+		} else {
+			fmt.Println("FAILED market order, ", orderMarket)
 		}
 	}
+}
+
+func (ft *FtxTrade) CheckBalanceUSD(subAcc string, client *ftx.FtxClient) (float64, error) {
+	sBalances, err := client.GetSubaccountBalances(subAcc)
+	equity := 0.0
+	if err != nil {
+		return equity, err
+	}
+	fmt.Println(sBalances)
+	if sBalances.Success {
+		for _, balance := range sBalances.Result {
+			if balance.Coin == "USD" {
+				equity = balance.Free
+			}
+			fmt.Println("Coin\t", balance.Coin, "\tfree:\t", balance.Free, "\ttotal:\t", balance.Total)
+		}
+	}
+
+	return equity, nil
+}
+
+func (ft *FtxTrade) GetLastPriceForMarket(market string, client *ftx.FtxClient) (float64, error) {
+	marketPrice := 0.0
+	candles, err := client.GetHistoricalPricesLatest(market, 60, 1)
+	if err != nil {
+		return marketPrice, err
+	}
+
+	if len(candles.Result) > 0 {
+		candle := candles.Result[0]
+		marketPrice = candle.Close
+	}
+
+	return marketPrice, nil
 }
