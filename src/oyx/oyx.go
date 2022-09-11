20b2d9cc769b0c60089e6ba84fc262fbc58cf4a2
Sander Mets <sandermets@live.com>
2022-09-11 23:03:18 +0300

    Draft plan in the form of comments

--- src/oyx/oyx.go
+++ src/oyx/oyx.go
@@ -28,7 +28,21 @@ func New(cfg config.Config, notif notification.Notification, appState state.Stat
 }
 
 func (o *Oyx) Run() {
+	// keep logic in separate functions
 	for {
+		// check if buy allowed
+		// get subaccount from conf
+		// get portfolio from conf
+		// check if any bids open for portfolio
+		// cancel expired bids
+		// get latest prices every minute
+		// calculate rocp from close for period
+		// check balances
+		// divide free balances
+		// if available funds and no bids:
+		// market buy some and set bids lower
+
+		// rinse and repeat
 
 		t := time.Now()
 		fmt.Println("Fine now", t)
